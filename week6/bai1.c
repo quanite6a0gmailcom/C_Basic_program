9d5461fdccb5ebf67f435b1bb01941e65906a87b
quanite6a0gmailcom <quanle12a0@gmail.com>
2021-11-09 11:01:27 +0700

    bai1.c+bai2.c

---
+++ week6/bai1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char name[20];
+    char phonenumber[20];
+    char email[30];
+}address;
+
+int main(){
+address sv[100];
+FILE *ptr;
+ptr=fopen()
+
+}
---
+++ week6/bai2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sequentinal_search(int *a,int count,int key){
+  int i;
+  for (i=0;i<count;i++)
+      if (key==a[i]) return i;
+    
+  return 0;
+}
+int main(){
+    int a[10];
+    int i;
+    int key;
+    int index;
+    for (i=0;i<11;i++){
+        if (i==10) 
+        {
+            printf("\nNhap linh canh :");
+            scanf("%d",&key);
+        }else{
+             printf("\nNhap phan tu thu %d cua mang:",i+1);
+             scanf("%d",&a[i]);  
+        }
+    }
+     index=sequentinal_search(a,10,key);
+     printf("%d",index);
+
+
+}
